97cf5ef677f24e90378e9d8345190bf8f8cb74e2
Nicholas Whitson <newhitson@gmail.com>
2017-07-09 22:45:40 -0700

    add store

--- frontend/reducer.js
+++ frontend/reducer.js
@@ -1,10 +1,17 @@
-const initialState = {
-  baseCurrency: "Please select",
-  rates: {}
-};
+
+const initialState = {baseCurrency: "Please select", rates: {}};
 
 const reducer = (state = initialState, action) => {
-  return state; // remove this and fill out the body of the reducer function
+  switch(action.type){
+    case "SWITCH_CURRENCY":
+      return {
+        baseCurrency: action.baseCurrency,
+        rates: action.rates
+      };
+    default:
+      return state;
+  }
+
 };
 
 export default reducer;
